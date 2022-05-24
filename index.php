b30de7f870611b9b3fff26b7169ca26e709b7754
Ananta kandel <kandelananta12@gmail.com>
2022-05-24 11:16:18 +0545

    initial homepage

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="./index.css" rel="stylesheet">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body class="bg-cyan-300  " >
+<navbar class="fixed inset-x-0 top-0 p-5 bg-white shadow md:flex md:items-center md:justify-between">
+  <div class="flex justify-evenly ">
+  <a href="index.php"><span class="text-2xl text-red-500 font-[poppins] cursor-pointer"><img class="h-14 mr-2 inline rounded-2xl " src="https://cdn.centraljersey.com/wp-content/uploads/sites/19/2021/10/vote-5676562_1920.jpg">VotIng</span></a>
+</div>
+<div>
+  <ul class= "md:flex md:items-center">
+  <li class="mx-4 my-6  "><a class=" text-xl hover:text-cyan-500 duration-75" href="home.php">HOME</a></li>
+  <li class="mx-4 my-6"><a class=" text-xl hover:text-cyan-500 duration-75" href="home.php">HELP</a></li>
+  <a href="register.php"> <button class="bg-cyan-400  rounded-xl text-white font-[poppins] duration-500 px-6 py-2 mx-4 hover:bg-cyan-500 ">REGISTER</button></a>
+  <a href="vote.php"><button class="bg-cyan-400  rounded-xl text-white font-[poppins] duration-500 px-6 py-2 mx-4 hover:bg-cyan-500 ">VOTE</button></a>
+    </ul>
+</div>
+</navbar>
+<section>
+<div class="flex img  flex-wrap ">
+<p>Let's vote</p>
+     <p>Make country beautiful , Choose right representative</p>
+  <img src="https://78.media.tumblr.com/5514169502a2bbd91f03769677878d13/tumblr_owl94kuuAm1vs8k5oo1_1280.gif"
+        class="object-cover  w-full h-full"
+        alt=""
+      />
+     <p>Let's vote</p>
+     <p>Make country beautiful , Choose right representative</p>
+</div>
+</div>
+</section>
+<footer class=" bg-white text-sm fixed inset-x-0 bottom-0 p-2 ">
+     <div class="m-auto w-1/2  ">
+        <p class="text-center"> &copy;Copyright @2022 , All Right Reserved to Ananta kandel <p>
+   </div>
+</footer>
+
+</body>
+</html>
+
+<?php
+
+?>
